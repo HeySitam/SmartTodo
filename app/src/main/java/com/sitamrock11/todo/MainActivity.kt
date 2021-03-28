3e41477dbb29fa824d094e927decd490176285a4
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-03-28 09:34:06 +0530

    search functionality till not working, have to improve

--- app/src/main/java/com/sitamrock11/todo/MainActivity.kt
+++ app/src/main/java/com/sitamrock11/todo/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.sitamrock11.todo
 
 import android.content.Intent
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sitamrock11.todo.Room.Todo
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -17,42 +22,167 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), IRVAdapter {
     val viewModel by lazy {
-        ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(TodoViewModel::class.java)
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(TodoViewModel::class.java)
     }
-     lateinit var todoList:List<Todo>
-    val rvAdapter=RVAdapter(this,this)
+     val todoList =ArrayList<Todo>()
+    val rvAdapter = RVAdapter(this, this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  val rvAdapter=RVAdapter(this,this)
-        rvTodo.apply{
-            layoutManager= LinearLayoutManager(this@MainActivity)
-            adapter=this@MainActivity.rvAdapter
+        //  val rvAdapter=RVAdapter(this,this)
+        rvTodo.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.rvAdapter
         }
-        viewModel.allTodos.observe(this, Observer{
-            rvAdapter.updateList(it)
+        initSwipe()
+        viewModel.allTodos.observe(this, Observer {
+            if (!it.isNullOrEmpty()) {
+                todoList.clear()
+                todoList.addAll(it)
+                rvAdapter.updateList(todoList)
+            }else{
+                todoList.clear()
+                rvAdapter.notifyDataSetChanged()
+            }
         })
         setSupportActionBar(toolbar)
         floatingActionButton.setOnClickListener {
             startActivity(Intent(this, TaskAddingActivity::class.java))
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+        val item = menu.findItem(R.id.search)
+        val searchView = item.actionView as SearchView
+        item.setOnActionExpandListener(object :MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+        })
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(!newText.isNullOrEmpty()){
+                    displayTodo(newText)
+                }
+                return true
+            }
+
+        })
+
         return super.onCreateOptionsMenu(menu)
+    }
+
+    fun displayTodo(newText: String = "") {
+        viewModel.allTodos.observe(this, Observer {
+            if(it.isNotEmpty()){
+                todoList.clear()
+                todoList.addAll(
+                    it.filter { todo ->
+                        todo.title.contains(newText,true)
+                    }
+                )
+                rvAdapter.notifyDataSetChanged()
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.history -> {
-                Toast.makeText(this@MainActivity, "Clicked on history button", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Clicked on history button", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
     override fun onDeleteBtnClicked(todo: Todo) {
-           viewModel.deleteTask(todo.id)
+        viewModel.deleteTask(todo.id)
+    }
+
+    fun initSwipe() {
+        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    Log.d("check",rvAdapter.getItemId(position).toString())
+                    viewModel.deleteTask(rvAdapter.getItemId(position))
+                    println(rvAdapter.getItemId(position))
+                }
+            }
+
+            override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val itemView = viewHolder.itemView
+                    val paint = Paint()
+                    val icon: Bitmap
+                    if (dX > 0) {
+                       // icon = BitmapFactory.decodeResource(resources, R.drawable.check_circle)
+                        paint.color = Color.parseColor("#388E3C")
+                        canvas.drawRect(
+                            itemView.left.toFloat(),
+                            itemView.top.toFloat(),
+                            itemView.left.toFloat() + dX,
+                            itemView.bottom.toFloat(),
+                            paint
+                        )
+//                        canvas.drawBitmap(
+//                            icon,
+//                            itemView.left.toFloat(),
+//                            itemView.top.toFloat() + (itemView.bottom.toFloat() - itemView.top.toFloat() - icon.height.toFloat()) / 2,
+//                            paint
+//                        )
+                    }
+                    itemView.translationX=dX
+                } else {
+
+                    super.onChildDraw(
+                        canvas,
+                        recyclerView,
+                        viewHolder,
+                        dX,
+                        dY,
+                        actionState,
+                        isCurrentlyActive
+                    )
+                }
+            }
+        }
+        val itemTouchHelper=ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(rvTodo)
     }
 }
--- app/src/main/java/com/sitamrock11/todo/RVAdapter.kt
+++ app/src/main/java/com/sitamrock11/todo/RVAdapter.kt
@@ -5,32 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.sitamrock11.todo.Room.Todo
 import kotlinx.android.synthetic.main.add_task.*
+import kotlinx.android.synthetic.main.list_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.random.Random.Default.nextInt
 
-class RVAdapter(private val context: Context, val listener:IRVAdapter): RecyclerView.Adapter<RVAdapter.TodoViewHolder>() {
-    private val allNotes = ArrayList<Todo>()
+class RVAdapter(private val context: Context, val listener: IRVAdapter) :
+    RecyclerView.Adapter<RVAdapter.TodoViewHolder>() {
+    val allNotes = ArrayList<Todo>()
 
-    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+   inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
         val tvCategory: TextView = itemView.findViewById(R.id.tvCategory)
         val tvDescription: TextView = itemView.findViewById(R.id.tvDescription)
         val tvDate: TextView = itemView.findViewById(R.id.tvDueDate)
         val tvDueTime: TextView = itemView.findViewById(R.id.tvDueTime)
-        val btnDelete:MaterialButton=itemView.findViewById(R.id.btnDelete)
+        val btnDelete: MaterialButton = itemView.findViewById(R.id.btnDelete)
+       fun bind() {
+           with(itemView) {
+               val colors = resources.getIntArray(R.array.random_color)
+               val randomColor = colors[Random().nextInt(colors.size)]
+               viewColorTag.setBackgroundColor(randomColor)
+           }
+       }
+
     }
-
+//Here overriding the function getItemId
+    override fun getItemId(position: Int): Long {
+        return allNotes[position].id
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        val viewHolder = TodoViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item, parent, false))
+        val viewHolder =
+            TodoViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item, parent, false))
 
-        viewHolder.btnDelete.setOnClickListener{
+        viewHolder.btnDelete.setOnClickListener {
             listener.onDeleteBtnClicked(allNotes[viewHolder.adapterPosition])
         }
+      //   id=allNotes[viewHolder.adapterPosition].id
         return viewHolder
     }
 
@@ -39,21 +58,22 @@ class RVAdapter(private val context: Context, val listener:IRVAdapter): Recycler
         holder.tvTitle.text = currentTodo.title
         holder.tvCategory.text = currentTodo.category
         holder.tvDescription.text = currentTodo.description
-        updateTime(holder,currentTodo)
-        updateDate(holder,currentTodo)
+        updateTime(holder, currentTodo)
+        updateDate(holder, currentTodo)
+        holder.bind()
     }
 
-    private fun updateDate(holder: TodoViewHolder,currentTodo:Todo) {
+    private fun updateDate(holder: TodoViewHolder, currentTodo: Todo) {
         //Sun, 14-Mar,2021
         val format = "EEE, d-MMM,yyyy"
         val date = SimpleDateFormat(format).format(Date(currentTodo.time))
         holder.tvDate.text = date
     }
 
-    private fun updateTime(holder: TodoViewHolder,currentTodo:Todo) {
+    private fun updateTime(holder: TodoViewHolder, currentTodo: Todo) {
         //02:49 pm
         val myTimeFormat = "hh:mm a"
-        val time = SimpleDateFormat(myTimeFormat).format( Date(currentTodo.time))
+        val time = SimpleDateFormat(myTimeFormat).format(Date(currentTodo.time))
         holder.tvDueTime.text = time
     }
 
@@ -61,12 +81,13 @@ class RVAdapter(private val context: Context, val listener:IRVAdapter): Recycler
         return allNotes.size
     }
 
-    fun updateList(newList: List<Todo>){
+    fun updateList(newList: List<Todo>) {
         allNotes.clear()
         allNotes.addAll(newList)
         notifyDataSetChanged()
     }
 }
-interface IRVAdapter{
-    fun onDeleteBtnClicked(todo:Todo)
+
+interface IRVAdapter {
+    fun onDeleteBtnClicked(todo: Todo)
 }
